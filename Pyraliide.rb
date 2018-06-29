9d086ded7caef2ddf260eeab1a1d1e49070fadc1
Luxilux <40703409+Luxilux@users.noreply.github.com>
2018-06-30 00:09:52 +0200

    Create Pyraliide.rb

---
+++ Pyraliide.rb
@@ -0,0 +1,18 @@
+def THPyramide()
+  
+
+  puts 'Bonjour, bienvenue dans ma super pyramide combien d etage veux tu ?'
+n=int(input("*"))
+for i in range(n): 
+  print(" "*(n-1-i)+"* "*(i+1))
+num= int(input("*"))
+for i in range (0,num)
+for j in range (0,num-i-1)
+   print ("*",end="")
+   print()
+ 
+  end
+end
+
+THPyramide()
+
