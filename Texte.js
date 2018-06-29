7ebb3b893eba57b838132645c18e446321f9f92e
Luxilux <40703409+Luxilux@users.noreply.github.com>
2018-06-30 00:11:33 +0200

    Create Texte.js

---
+++ Texte.js
@@ -0,0 +1,59 @@
+.Introduction1
+{color: red
+}
+
+.Mother
+{color: green
+
+}
+h1+em
+{color: green}
+p+em
+{color: green
+}
+*
+{font-size: 1em;}
+
+
+
+p
+{font-family: Arial
+border:3px
+padding:15px
+}
+
+
+body
+{margin: 0px;
+padding:0px;
+font-size:1em
+}
+
+
+*{
+font-size: 1.1em;
+  text-align: justify;
+  }
+h1
+{text-align:center;}
+
+@charset"utf-8";
+
+
+.button_container {
+width: 100%;
+text-align : center;
+}
+
+#buton_modal {
+     height: 50px
+      width: 300px;
+     background-color:#34495e;
+     color: #8e44ad;
+     font-family: helvetica;
+     font-size: 20px;
+     border: none;
+     cursor: pointer;
+     margin-top: 200px;
+
+      }
